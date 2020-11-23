5a24f018199e3b78b249754059feb0b5e03b0aa7
Eldor99 <eldor1999@protonmail.com>
2020-11-23 17:25:12 +0500

    added User  Component

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 import React, {Component} from 'react';
 import './App.css';
 import Navbar from './components/layout/Navbar';
-import UserItem from './components/UserItem';
+import Users from './components/Users';
 
 class App extends Component {
     render() {
         return (
             <div className = "App" >
             <Navbar title='Github Finder' icon="fab fa-github" />
-            <UserItem />
+            <div className='container' > <Users /> </div>
             </div>
         );
     }
