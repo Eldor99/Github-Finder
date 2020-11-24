386aaf38d4b9dc46bf85cdbcd73a03fee434c808
Eldor99 <eldor1999@protonmail.com>
2020-11-24 14:23:45 +0500

    passing props and Search

--- src/App.js
+++ src/App.js
@@ -16,14 +16,23 @@ class App extends Component {
 
     const res = await axios.get(`https://api.github.com/users?client_id=${process.env.REACT_APP_API_KEY}&client_secret=${process.env.REACT_APP_API_CLIENT_SECRET}`)
 
-    this.setState({ users: res.data, loading: false})
+    this.setState({ users: res.data, loading: false});
   }
+
+// Search Github Users
+  searchUsers = async (text) => {
+    this.setState({ loading: true });
+    const res = await axios.get(`https://api.github.com/search/users?q=${text}&client_id=${process.env.REACT_APP_API_KEY}&client_secret=${process.env.REACT_APP_API_CLIENT_SECRET}`)
+
+    this.setState({ users: res.data.items, loading: false});
+  }
+
     render() {
         return (
             <div className = "App" >
             <Navbar title='Github Finder' icon="fab fa-github" />
             <div className='container' >
-             <Search />
+             <Search searchUsers={this.searchUsers} />
              <Users loading={this.state.loading} users={this.state.users} /> 
             </div>
             </div>
